89ec0ce811cf2fd7cd8d2c2b08e2efad44513453
Michael Axelson <axelmichael11@gmail.com>
2017-10-12 17:57:39 -0700

    template and styling are complete

---
+++ src/components/AlbumDetail/index.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import {View, Text, Image, Linking} from 'react-native'
+import axios from 'axios'
+
+
+import Card from '../Card'
+import CardSection from '../CardSection'
+import Button from '../button'
+
+
+const AlbumDetail = ({album}) => {
+  const {title, artist, thumbnail_image, image, url} = album
+
+  const {headerContentStyle,
+    thumbnailStyle,
+    headerTextStyle,
+    thumbnailContainerStyle,
+    imageStyle,
+  } = styles
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+        <Image
+        style={thumbnailStyle}
+        source={{ uri: thumbnail_image}}
+        />
+        </View>
+        <View style={headerContentStyle}>
+            <Text style={headerTextStyle}>
+            {title}
+            </Text>
+            <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle}
+
+        source={{uri: image}}/>
+      </CardSection>
+      <CardSection>
+        <Button onPress={()=> Linking.openURL(url)}>
+        Buy Now
+        </Button>
+      </CardSection>
+    </Card>
+
+  )
+}
+
+const styles = {
+  headerContentStyle:{
+    flexDirection: 'column',
+    justifyContent:'space-around',
+  },
+  thumbnailStyle: {
+    height:50,
+    width: 50,
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+    marginLeft: 10,
+  },
+  imageStyle: {
+    height:300,
+    flex: 1,
+    width: null,
+  }
+}
+
+export default AlbumDetail
--- src/index.ios.js
+++ src/index.ios.js
@@ -5,7 +5,7 @@ import Header from './components/header'
 import AlbumList from './components/AlbumList'
 
 export default App = () => (
-    <View>
+    <View style={{flex:1}}>
       <Header headerText={'Albums'}/>
       <AlbumList />
     </View>
